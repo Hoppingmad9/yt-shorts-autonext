9839cc1de3b5ed927867ebfd95f1800380fed6ec
DesignChapman <timfc1+github@gmail.com>
2023-03-03 20:55:39 +0000

    wrapped code in document.onload(), added extension boilerplate

--- main.js
+++ main.js
@@ -1,21 +1,24 @@
-var x = document.getElementById('progress-bar-line');
-let count = 1;
-let progress = 0;
-var observer = new MutationObserver(function(mutations) {
-  mutations.forEach(function(mutation) {
-    if (mutation.type === "attributes") {
-      let val = parseInt(mutation.target.getAttribute('aria-valuenow'))
-      if (val < progress) {
-        let nextVidEl = document.getElementById(count);
-        nextVidEl.scrollIntoView();
-        var x = document.getElementById('progress-bar-line');
-        count++;
-        progress = 0
+document.onload( ()=>{
+  var x = document.getElementById('progress-bar-line');
+  let count = 1;
+  let progress = 0;
+  console.log(this)
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.type === "attributes") {
+        let val = parseInt(mutation.target.getAttribute('aria-valuenow')) /* Current position in video*/
+        if (val < progress) {
+          let nextVidEl = document.getElementById(count);
+          nextVidEl.scrollIntoView();
+          var x = document.getElementById('progress-bar-line');
+          count++;
+          progress = 0
+        }
+        progress = val;
       }
-      progress = val;
-    }
+    });
   });
-});
-observer.observe(x, {
-  attributes: true
-});
+  observer.observe(x, {
+    attributes: true
+  });
+})
